6dc7e143bf51d74332dcb1138a211ec8c3dab002
Trollock <jrs566@cornell.edu>
2023-01-13 16:12:49 +1100

    attempting to update website

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,16 +1,49 @@
 var store = [{
+        "title": "Dynamic Energy Budget Models",
+        "excerpt":"Dynamic Energy Budget Models Dynamic Energy Budget theory (DEB) is a theory of metabolism, i.e. of the chemical transformations of living things as they develop, grow, reproduce and age throughout their life cycle, given their food intake and body temperature. It was developed primarily by Prof. Bas Kooijman in collaboration...","categories": [],
+        "tags": [],
+        "url": "/models/Dynamic-Energy-Budget-Models",
+        "teaser": null
+      },{
         "title": "Ectotherm Models",
-        "excerpt":"Ectotherm Models   Coming Soon    ","categories": [],
+        "excerpt":"Ectotherm Models The general ectotherm model The NicheMapR package includes a comprehensive algorithm for the complete heat, water and nutritional budget of an ectotherm to be solved, as described in detail in Kearney and Porter (2019). The program comprises a set of FORTRAN subroutines that encode calculations of the steady-state...","categories": [],
         "tags": [],
-        "url": "http://localhost:4000/models/Ectotherms-Models",
-        "teaser":null},{
-        "title": "MicroClimate Models",
-        "excerpt":"MicroClimate Models    Coming Soon   ","categories": [],
+        "url": "/models/Ectotherm-Models",
+        "teaser": null
+      },{
+        "title": "Endotherm Models",
+        "excerpt":"Endotherm Models Computing heat budgets for endotherms involves using the same principles as for ectotherms, but instead of solving for core temperature one typically solves for the metabolic rate or water loss rate, depending on whether the animal is in a hot or cold environment. There are two broad modelling...","categories": [],
         "tags": [],
-        "url": "http://localhost:4000/models/MicroClimate-Models",
-        "teaser":null},{
-        "title": "Hello World",
-        "excerpt":"This is a filler post.   This will be deleted eventually.  ","categories": [],
-        "tags": ["testing"],
-        "url": "http://localhost:4000/Hello-World/",
-        "teaser":null}]
+        "url": "/models/Endotherm-Models",
+        "teaser": null
+      },{
+        "title": "Microclimate Models",
+        "excerpt":"Microclimate Models Microclimates represent the physical environments experienced by organisms. They are a necessity for mechanistic niche modelling because it is the environment experienced at the scale of the individual that needs to be provided to the equations of energy and mass balance. Microclimate predictions are also useful for a...","categories": [],
+        "tags": [],
+        "url": "/models/Microclimate-Models",
+        "teaser": null
+      },{
+        "title": "Plant Models",
+        "excerpt":"Plant Models Presently, there is only limitied plant modelling capacity in NicheMapR. The microclimate model provides many of the environmental inputs required for plant modelling, including soil temperature and moisture, and wavelength-specific irradiance (parameter 'lamb' set to 1 returns the wavelength-specific solar radiation output). Also, the soil moisture algorithm incorporates...","categories": [],
+        "tags": [],
+        "url": "/models/Plant-Models",
+        "teaser": null
+      },{
+        "title": "The NicheMapR website is launched",
+        "excerpt":"The first news is … that this website has been launched! We hope that this provides a useful entry point into modelling with NicheMapR.   Special thanks to Davud Porter for putting the structure of the website together.     ","categories": [],
+        "tags": ["NicheMapR","on","the","web"],
+        "url": "/NicheMapR_website_launched/",
+        "teaser": null
+      },{
+        "title": "NicheMapR and snow",
+        "excerpt":"This paper in Global Ecology and Biogeography https://doi.org/10.1111/geb.13100 provides a detailed explanation and test of the snow model in the NicheMapR microclimate model. It uses the model together with the gridMET historical weather grids for the USA to assess the consequences of historical and future warming on snow and how...","categories": [],
+        "tags": ["snow","modelling"],
+        "url": "/NicheMapR_snow_model/",
+        "teaser": null
+      },{
+        "title": "NicheMapR Shiny apps",
+        "excerpt":"There are now Shiny apps for the for the microclimate and ectotherm models that run using a monthy long-term average climatology for the globe (function ‘micro_global’ in the package). It is a good way to get a feel for what the models do and can be used for teaching exercises....","categories": [],
+        "tags": ["Shiny"],
+        "url": "/NicheMapR_shiny_apps/",
+        "teaser": null
+      }]
